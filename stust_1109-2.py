30aad59593a30e68b33d60f9538d629c8b16921c
蕭語萱 <112915171+yuhsuan02@users.noreply.github.com>
2023-11-09 12:03:52 +0800

    Stust_2023

---
+++ stust_1109-2.py
@@ -0,0 +1,7 @@
+def calculation(a, b):
+    return a + b, a - b
+
+# get result in tuple format
+# unpack tuple
+add, sub = calculation(40, 10)
+print(add, sub)
---
+++ stust_1109-4.py
@@ -0,0 +1,14 @@
+def outer_fun(a, b):
+    square = a ** 2
+
+    # inner function
+    def addition(a, b):
+        return a + b
+
+    # call inner function from outer function
+    add = addition(a, b)
+    # add 5 to the result
+    return add + 5
+
+result = outer_fun(5, 10)
+print(result)
